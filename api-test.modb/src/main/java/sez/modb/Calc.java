eb33af47456b55f31425819acee8bbbf0974bfd1
lizardbuzzard <lizardbuzzard@gmail.com>
2015-08-23 22:27:32 +0300

    Add modb module.

---
+++ api-test.modb/src/main/java/sez/modb/Calc.java
@@ -0,0 +1,20 @@
+package sez.modb;
+
+/**
+ * Library class
+ *
+ */
+public class Calc
+{
+    /**
+     * Will return the sum of 2 arguments.
+     * @param a - first parameter.
+     * @param b - second parameter.
+     * @return sum of a+b.
+     */
+    public int doSum(int a, int b){
+        return a+b;
+    }
+
+
+}
