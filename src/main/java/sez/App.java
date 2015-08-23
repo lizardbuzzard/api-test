042f2fe76c9ea7c25baf0f2dbfd26af2612d893c
lizardbuzzard <lizardbuzzard@gmail.com>
2015-08-23 20:13:47 +0300

    Usage of ModB module.

--- src/main/java/sez/App.java
+++ src/main/java/sez/App.java
@@ -1,5 +1,7 @@
 package sez;
 
+import sez.modb.Calc;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int sum;
+        Calc calc = new Calc();
+        sum = calc.doSum(5,7);
+        System.out.println("Sum = "+sum);
     }
 }
